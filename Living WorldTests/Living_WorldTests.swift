2fd2851995644851605e710a0526f7eed67f34ed
Javier Rivarola <javierrivarola@tacuaratech.com>
2015-10-15 15:34:31 -0300

    Initial Commit

---
+++ Living WorldTests/Living_WorldTests.swift
@@ -0,0 +1,36 @@
+//
+//  Living_WorldTests.swift
+//  Living WorldTests
+//
+//  Created by Javier Rivarola on 10/15/15.
+//  Copyright © 2015 Javier Rivarola. All rights reserved.
+//
+
+import XCTest
+@testable import Living_World
+
+class Living_WorldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
